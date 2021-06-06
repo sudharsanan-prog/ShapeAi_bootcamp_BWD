c4683a43ff414d0e34e9433f7cece572b7d9d18c
sudharsanan-prog <85428326+sudharsanan-prog@users.noreply.github.com>
2021-06-06 21:58:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <note/>
+      <note/>
+      <note/>
+      <note/>
+    </div>
+  );
+}
+
+export default App;
---
+++ note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an Amazing bootcamp.I have learned a lot.thank you so much for
+        this wonderfull bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
